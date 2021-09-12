b0d9bfc3ee54e8bad0ec770fada032ed8bf9367e
+ <brandi.ude@gmail.com>
2021-09-12 00:42:22 -0400

    Completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Write your solution here!
+const cats =["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendCat() {
+    cats.push("Ralph")
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob")
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift()
+}
+
+const newcat = ["Broom"];
+function appendCat() {
+    let newcat=([...cats, "Broom"]);
+    return newcat;
+}
+
+const anothernewcat = ["Arnold"];
+function prependCat () {
+    let anothernewcat = ([ "Arnold",...cats]);
+    return anothernewcat;
+}
+
+function removeLastCat() {
+    return cats.slice(0, cats.length - 1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
+        
